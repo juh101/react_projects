8ebe4dd2240392f0b1133f9777f16fc9eac06939
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-06-27 18:40:19 +0530

    updated

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,12 @@
-
+import Chai from './chai'
+// the imported file should have capitalized letter
 function App() {
   return (
+    // will only return one element
+    <>
     <h1>Chai aur React</h1>
+    <Chai/>
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,17 @@
+import { StrictMode } from "react"
+import Chai from "./chai.jsx"
+
 
 function App() {
 
   return (
-    <h1>chai and code eith vite</h1>
+    // can only return one element
+    <>
+    <h1>chai and code with vite</h1>
+    <StrictMode>
+    <Chai/>
+    </StrictMode>
+    </>
   )
 }
 
