e62f436050d9f380aac3bd7bc7e34329c5d8d59a
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-06-27 19:19:11 +0530

    customReact

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -12,11 +12,22 @@ const reactElement = {
 const container = document.querySelector('#root');
 
 function customRender(reactEle , mainContainer){
-    const domEle = document.createElement(reactEle.type) // reactEle.type is 'a'
+    /*const domEle = document.createElement(reactEle.type) // reactEle.type is 'a'
     domEle.innerHTML = reactEle.children //reactEle.children is 'click to visit google'
     domEle.setAttribute('href' , reactEle.props.href)
     domEle.setAttribute('target' , reactEle.props.target)
-    mainContainer.appendChild(domEle);
+    mainContainer.appendChild(domEle);*/
+
+    // creating a more loop based code
+
+    const domEle = document.createElement(reactEle.type)
+    domEle.innerHTML = reactEle.children
+    for(const prop in reactEle.props){
+        if(prop === 'children') continue;
+        domEle.setAttribute(prop , reactEle.props[prop])
+    }
+
+    mainContainer.appendChild(domEle)
 
 }
 customRender(reactElement , container)
