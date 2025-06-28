edbf69986b37af935b5ba4e5adb07a437a98bafa
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-06-28 13:45:21 +0530

    updated

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,14 +3,14 @@ import Chai from "./chai.jsx"
 
 
 function App() {
-
+  const username = "juhi"
+  const type = typeof(App)
   return (
     // can only return one element
+
     <>
-    <h1>chai and code with vite</h1>
-    <StrictMode>
+    <h2>chai and code with vite my username is {username} {type}</h2>
     <Chai/>
-    </StrictMode>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+import {jsx as _jsx} from "react/jsx-runtime.js" //directly create elements from jsx
+
+
+function MyApp(){
+  return(
+    <h1>THIS IS MY APP!!</h1>
+  )
+} // directly inserting the function also works
+
+const reactElement = {
+    type:'a',
+    props: {
+        href: 'https://www.google.com/' ,
+        target: '_blank'
+    },
+    children: 'click to visit google'
+}
+const AnotherEle = (
+  <a href="https://www.google.com/" target='_blank'>visit google</a>
+)
+const sec = typeof AnotherEle //object
+const anotheruser = "this is me"
+const ReactEle = React.createElement(
+  'a',
+  {href: "https://www.google.com/" , target: '_blank'},
+  'click me to visit google ',
+  // anotheruser , sec // these variables are injected
+  // AnotherEle
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    
+    // <MyApp />
+    <App/>
+    // AnotherEle
+    // ReactEle
 )
