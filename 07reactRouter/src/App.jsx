00b506887afcfc40594763d699b2c9eb5999165c
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-07-04 15:08:31 +0530

    update

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header/header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+      <>
+      
+      </>
+
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Link , NavLink} from 'react-router-dom'
+// Link is better than <a> tag as using a tag causes a full reload and loses app state
+export default function Header() {
+    return (
+        <header className="shadow sticky z-50 top-0">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Link to="/" className="flex items-center">
+                        <img
+                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            // src="https://imgs.search.brave.com/4HMivhOdphcfQO8NGsD5sxgaX39YqesQ9Tp7iEGGQ00/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2FhLzFh/L2IzL2FhMWFiMzcw/YmNkODZhYzNkODFl/ZDRhNjRkMWY3MzJm/LmpwZw"
+                            className="mr-3 h-12"
+                            alt="Logo"
+                        />
+                    </Link>
+                    <div className="flex items-center lg:order-2">
+                        <Link
+                            to="#"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            to="#"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get started
+                        </Link>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive? "text-orange-700":"text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        //isActive checks if the current link matches to the "to" link of NavLink, it is a prop/parameter passed to className function
+                                    }
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
