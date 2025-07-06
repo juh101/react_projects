e103b09dd8583a92e7be96cffaf86710a47be2e1
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-07-06 23:03:46 +0530

    completed

--- 09themeswitch/src/App.jsx
+++ 09themeswitch/src/App.jsx
@@ -1,22 +1,34 @@
-import { useState } from 'react'
+import { useState,useEffect, useContext } from 'react'
 import './App.css'
-import { ThemeProvider } from './contexts/theme'
+import { ThemeContext , ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [theme , setTheme] =useState('light')
+  const [themeMode , setTheme] =useState('light')
 
+  const lightTheme = () => {
+    setTheme('light')
+  }
+  const darkTheme = () => {
+    setTheme('dark')
+  }
 
-  
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+    console.log(themeMode);
+  }, [themeMode])
   return (
     <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themeButton */}
+            <ThemeBtn/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            {/* Card */}
+            <Card/>
           </div>
         </div>
       </div>
