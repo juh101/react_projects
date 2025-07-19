2bbe77fa549d03cce32ad152ae950b40530814d0
Juhi Ahuja <juhiahuja2006@gmail.com>
2025-07-19 13:26:51 +0530

    layout for authentication added

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import React from 'react'
 
 function Protected({
     children, authentication = true
